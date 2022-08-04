f9d2bfa17f941911ad5902cf4ed7081249e1bd81
Anastasiya Kim <anastasiyapotard@gmail.com>
2022-08-04 21:20:11 +0200

    routes for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :superpowers do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reservations
+    resources :reservations, only: [ :new, :create ]
   end
+  resources :reservations, only: [ :destroy, :edit, :update ]
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
