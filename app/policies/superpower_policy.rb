ab03ade2904378e0219019829a4a9a4fe6556751
Sen Cao <caosen.cn@gmail.com>
2022-08-04 16:58:52 +0300

    destry action added to superpower model

---
+++ app/policies/superpower_policy.rb
@@ -0,0 +1,25 @@
+class SuperpowerPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user == record.user
+  end
+
+  def destroy?
+    user == record.user
+  end
+
+end
