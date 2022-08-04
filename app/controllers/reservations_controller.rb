a720355cee85a010da294f7663ad89102ae7405f
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-08-04 21:53:03 +0300

    reservations destroy method done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,14 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:show, :edit, :update, :destroy]
 
+  def destroy
+    @reservation.destroy
+    redirect_to reservations_path
+  end
+
+  private
+
+  def set_superpower
+    @reservation = Reservation.find(params[:id])
+  end
 end
