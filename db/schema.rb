ff7c7f2a5ad18e427e937a7e648ada4b35950b9d
Sen Cao <caosen.cn@gmail.com>
2022-08-06 16:47:38 +0300

    yes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_191552) do
 ActiveRecord::Schema.define(version: 2022_08_06_092256) do
-ActiveRecord::Schema.define(version: 2022_08_04_183332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +27,6 @@ ActiveRecord::Schema.define(version: 2022_08_04_183332) do
     t.index ["superpower_id"], name: "index_reservations_on_superpower_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
-
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
@@ -69,6 +66,4 @@ ActiveRecord::Schema.define(version: 2022_08_04_183332) do
   add_foreign_key "reservations", "users"
   add_foreign_key "reviews", "reservations"
   add_foreign_key "superpowers", "users"
-end
-end
 end
