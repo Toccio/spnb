d1b63245cf783fd187e7ad0990e93cbd91ff00ee
luca visaggio <luca.t.visaggio@gmail.com>
2022-08-06 15:23:54 +0200

    show page done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,29 +9,40 @@ class ReservationsController < ApplicationController
   def new
     @superpower = Superpower.find(params[:superpower_id])
     authorize @superpower
-
     @reservation = Reservation.new
   end
 
   def create
-
     @reservation = Reservation.new(reservation_params)
-
     @superpower = Superpower.find(params[:superpower_id])
-
     @reservation.superpower = @superpower
     @reservation.user = current_user
+    # @reservation.check_in = @reservation.check_in.strftime('%a, %d %b %Y')
+    # @reservation.check_out = @reservation.check_out.strftime('%a, %d %b %Y')
 
-
+    # raise
     if @reservation.save
-      redirect_to superpower_path(@superpower)
+      redirect_to reservation_path(@reservation)
     else
       render :new
     end
     authorize @reservation
-
   end
 
+  # check_out: Tue, 09 Aug 2022,
+
+def show
+  authorize @reservation
+end
+
+def edit
+end
+
+# def update
+#   @reservation.update(reservation_params)
+#   redirect_to reservation_path(@reservation)
+#     # - CHECK THE PATH
+# end
 
   def destroy
     @reservation.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :superpowers do
     resources :reservations, only: [ :new, :create ]
   end
-  resources :reservations, only: [ :destroy, :edit, :update, :index]
+  resources :reservations, only: [ :destroy, :edit, :update, :index, :show]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
