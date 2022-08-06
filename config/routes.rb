5ebf3c27b85a57495ba06e869e1c0c59bba805ac
Anastasiya Kim <anastasiyapotard@gmail.com>
2022-08-06 16:09:53 +0200

    debug2

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :superpowers do
-    resources :reservations, only: [ :new, :create ]
-    collection do
-      get :search
+    resources :reservations, only: [ :new, :create ] do
+      resources :reviews, only: [:create, :index, :show]
     end
   end
+  resources :reservations, only: [ :destroy, :edit, :update, :index, :show]
 
-  resources :reservations, only: [ :destroy, :edit, :update ] do
-    resources :reviews, only: [:create, :index, :show]
   end
 
-  resources :reservations, only: [ :destroy, :edit, :update, :index]
-end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -67,3 +67,5 @@ ActiveRecord::Schema.define(version: 2022_08_06_092256) do
   add_foreign_key "reviews", "reservations"
   add_foreign_key "superpowers", "users"
 end
+end
+end
