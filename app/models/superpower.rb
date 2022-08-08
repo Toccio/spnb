0d40d387e988cbe2fd48cbc841b4552fcd5b2dd8
luca visaggio <luca.t.visaggio@gmail.com>
2022-08-08 10:41:09 +0200

    hope

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,4 +1,4 @@
 class Superpower < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   has_many :reservations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :superpowers do
-<<<<<<< HEAD
-    resources :reservations, only: [ :new, :create, :show] do
-=======
     resources :reservations, only: [ :new, :create, :edit, :update ] do
->>>>>>> fa644fda96df1a5263f6720f7113139712ec1c48
       resources :reviews, only: [:create, :index, :show]
     end
     resources :reservations, only: [ :destroy, :edit, :update, :index, :show]
   end
-<<<<<<< HEAD
-  resources :reservations, only: [ :destroy, :edit, :update, :index ]
-=======
 end
 
->>>>>>> fa644fda96df1a5263f6720f7113139712ec1c48
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
