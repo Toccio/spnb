a066a2dd7967cd0c3b715859998ac674877d7d16
luca visaggio <luca.t.visaggio@gmail.com>
2022-08-09 20:00:26 +0200

    debugging the reservation form in show

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -15,6 +15,7 @@ class SuperpowersController < ApplicationController
 
   # GET / superpower / id
   def show
+    @reservation = Reservation.new
     # @superpower = Superpower.find(params[:id])
     # authorize @superpower
     # achieved with before_action
