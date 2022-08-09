cf76c4856b172748d37b87c06e8f37a53f16f3f9
Anastasiya Kim <anastasiyapotard@gmail.com>
2022-08-09 21:20:26 +0200

    search is done

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -4,4 +4,10 @@ class Superpower < ApplicationRecord
   has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_superpower_name,
+    against: [ :name, :superpower_name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
