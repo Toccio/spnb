4aea46e77d2e74ae00d28c7d2c4d64c14a2bdcd7
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-08-13 13:18:27 +0300

    users profile page done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
-    @reservations = User.reservations
-    @superpowers = User.reservations
+    # @reservations = User.reservations
+    # @superpowers = User.reservations
+    authorize @user
   end
 end
